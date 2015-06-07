9fbb037b6187d301c9e0a737ce5d3ca5573db80c
jhancock93 <jhancock93@users.noreply.github.com>
2015-06-07 17:05:57 -0400

    Updated

--- plot3.R
+++ plot3.R
@@ -1,18 +1,13 @@
 library(graphics)
 source("power.R") #power.R contains the code for reading and cleaning the data.
 
-plot3_draw <- function(power)
+plot3 <- function(power)
 {
+    dev <- png("plot3.png", width=480, height=480)
     plot(power$DateTime, power$Sub_metering_1, type="l", main="", ylab = "Energy sub metering", xlab="")
     lines(power$DateTime, power$Sub_metering_2, col="red")
     lines(power$DateTime, power$Sub_metering_3, col="blue")
     legend("topright", lty=1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-}
-
-plot3 <- function(power)
-{
-    dev <- png("plot3.png", width=480, height=480)
-    plot3_draw(power)
     dev.off()
 }
 
